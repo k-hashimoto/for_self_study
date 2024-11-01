1c21c29177fe387e20213aaf39eeb96d5b59f473
k-hashimoto <kimiaki.hashi@gmail.com>
2024-11-02 07:39:07 +0900

    一応ジニ係数を計算出来る

--- src/trial_dtree.rs
+++ src/trial_dtree.rs
@@ -4,104 +4,85 @@ fn print_type_of<T>(_: &T) {
     println!("Type: {}", type_name::<T>());
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Sample {
     label: usize,
     features: Vec<f64>
 }
-
-fn split_x_sample(samples: &Vec<Sample>, threshold: f64, split_axis: usize) -> (Vec<Sample>, Vec<Sample>)
-{
-    // Sample構造体のvectorをSampleの中の値に応じて2つに分割して、2つのSample構造体のvectorを返す
-    // 渡ってきた変数: Sample構造体のvector
-
-    // 処理
-    // vectorからsampleを取り出す
-    // vectorの中のSampleの値を参照する。ここはread-onlyで良い
-    // しきい値以上ならそのsampleを別のvectorに格納する
-
-    (
-        samples.iter().filter_map( | sample | {
-            // vec![1.0, 3.5, 2.2, 4.0, 0.5] の中からthreshold以上の要素を取り出す
-            let filtered_features: Vec<_> = sample.features.iter().cloned().filter(|&x| x >= threshold).collect();
-            if !filtered_features.is_empty() {
-                Some(
-                    Sample {
-                        label: sample.label,
-                        features: filtered_features,
-                    }
-                )
-            } else {
-                None
-            }
-        }).collect(),
-
-        samples.iter().filter_map( | sample | {
-            // vec![1.0, 3.5, 2.2, 4.0, 0.5] の中からthreshold以上の要素を取り出す
-            let filtered_features: Vec<_> = sample.features.iter().cloned().filter(|&x| x < threshold).collect();
-            if !filtered_features.is_empty() {
-                Some(
-                    Sample {
-                        label: sample.label,
-                        features: filtered_features,
-                    }
-                )
-            } else {
-                None
-            }
-        }).collect()
-    )
-    // let mut return_sample_a = Vec::new();
-    // let mut return_sample_b = Vec::new();
-    // for v in vector {
-    //     let _sample = v;
-    //     if _sample.features[split_axis] > threshold {
-    //         return_sample_a.push(_sample);
-    //     } else {
-    //         return_sample_b.push(_sample);
-    //     }
-    // }
-    //    (return_sample_a, return_sample_b)
+impl Sample {
+    fn contain_value(&self, threshold: f64, split_axis: usize) -> bool {
+        // featuresにthreshold以上の値がふくまれているか？
+        if self.features[split_axis] > threshold {
+            true
+        } else {
+            false
+        }
+    }
+    fn check_label(&self, value: usize) -> bool {
+        if self.label == value {
+            true
+        } else {
+            false
+        }
+    }
 }
 
-// fn filter_by_label(vector: &Vec<Sample>, label: usize) -> Vec<Sample>
-// {
-//     let mut return_sample = Vec::new();
-//     for v in vector {
-//         let _sample = v;
-//         if _sample.label == label {
-//             return_sample.push(_sample);
-//         }
-//     }
-//     return_sample
-// }
+fn split_samples(samples: &Vec<Sample>, threshold: f64, split_axis: usize) ->  (Vec<Sample>, Vec<Sample>) {
+    (
+        samples.iter().filter( | sample | {
+            sample.contain_value(threshold, split_axis)
+        }).cloned().collect(),
 
-fn impurity(all_samples: &Vec<Sample>, split_axis: usize) -> f64
+        samples.iter().filter( | sample | {
+            !sample.contain_value(threshold, split_axis)
+        }).cloned().collect()
+    )
+}
+
+fn filter_by_label(samples: &Vec<Sample>, label: usize) -> Vec<Sample>{
+    samples.iter().filter( | sample | {
+        sample.check_label(label)
+    }).cloned().collect()
+}
+
+fn impurity(all_samples: &Vec<Sample>, split_value: f64, split_axis: usize) -> f64
 {
     // 不純度を計算する
-    print_type_of(&all_samples);
+
     // 1. しきい値で分割する
-    let (split_sample_a, split_sample_b) = split_x_sample(&all_samples, 3.0, split_axis);
-    print_type_of(&split_sample_a);
+    let (split_sample_a, split_sample_b) = split_samples(&all_samples, split_value, 0);
 
-    // // 2. ラベルで分割する
-    // let split_sample_a_0 = filter_by_label(&split_sample_a, 0);
-    // let split_sample_a_1 = filter_by_label(&split_sample_a, 1);
+    // println!("split_sample_a = {:?}", split_sample_a);
+    // println!("split_sample_b = {:?}", split_sample_b);
 
-    println!("{:?}", split_sample_a);
+    // 2. ジニ係数計算のために、ラベルで分割する
+    let split_sample_a_0 = filter_by_label(&split_sample_a, 0);
+    let split_sample_a_1 = filter_by_label(&split_sample_a, 1);
+    let split_sample_b_0 = filter_by_label(&split_sample_b, 0);
+    let split_sample_b_1 = filter_by_label(&split_sample_b, 1);
 
-    // gini(split_sample_a, );
-    -1.
+    // println!("split_sample_a_1 = {:?}" , split_sample_a_1);
+    // println!("split_sample_a_0 = {:?}" , split_sample_a_0);
+
+    // 3. 分割したエリアごとにジニ係数を計算する
+    let gini_a = gini(&split_sample_a_0, &split_sample_a_1);
+    let gini_b = gini(&split_sample_b_0, &split_sample_b_1);
+
+    // 4. データ点の数で重みを計算し、平均をとる
+    let split_area_size_a = split_sample_a.len() as f64;
+    let split_area_size_b = split_sample_b.len() as f64;
+    let full_size = split_area_size_a + split_area_size_b;
+
+    // 5. この分割での全データをもとにした平均のジニ係数
+    (split_area_size_a / full_size) * gini_a + (split_area_size_b / full_size) * gini_b
 }
 
-fn gini(sample_a: &Vec<f64>, sample_b: &Vec<f64>) -> f64
+fn gini(sample_a: &Vec<Sample>, sample_b: &Vec<Sample>) -> f64
 {
     // ジニ係数の計算
-    let full_sample   = sample_a.clone();
-    let n_full_sample = full_sample.len() as f64;
     let n_sample_a    = sample_a.len() as f64;
     let n_sample_b    = sample_b.len() as f64;
-
+    let n_full_sample = n_sample_a + n_sample_b;
     1. - (
         ((n_sample_a / n_full_sample).powi(2)) + ((n_sample_b / n_full_sample).powi(2))
     )
@@ -112,21 +93,12 @@ fn main()
     let all_samples =  vec![
         Sample { label: 0, features: vec![1.0, 40.0]},
         Sample { label: 0, features: vec![2.0, 30.0]},
-        Sample { label: 0, features: vec![3.0, 10.0]},
+        Sample { label: 0, features: vec![3.1, 10.0]},
         Sample { label: 1, features: vec![5.0, 30.0]},
         Sample { label: 1, features: vec![8.0, 20.0]},
         Sample { label: 1, features: vec![4.0, 60.0]}
     ];
 
     println!("# 簡単な決定木アルゴリズム");
-    let sample_a = vec![1., 2., 3., 5., 10., 2.];
-    let sample_b = vec![40., 50., 60., 10., 30.,70.,];
-
-    let gini_c = gini(&sample_a, &sample_b);
-    println!("gini = {:.4}", gini_c);
-    //    let (split_sample_a, split_sample_b) = split_x_sample(&all_samples, 3.0, 0);
-
-    impurity(&all_samples, 0);
-
-
+    println!("{}", impurity(&all_samples, 3.0, 0));
 }
