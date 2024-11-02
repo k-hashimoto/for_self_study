89e6225eef744e474b6ee30dab35481485f53091
k-hashimoto <kimiaki.hashi@gmail.com>
2024-11-02 17:27:03 +0900

    csvから配列に格納

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -1,4 +1,8 @@
 #[allow(dead_code)]
+use csv::ReaderBuilder;
+use std::error::Error;
+use std::fs::File;
+
 pub fn mean_normal_dist(vector: &Vec<f64>) -> f64 {
     vector.iter().sum::<f64>() / vector.len() as f64
 }
@@ -30,4 +34,23 @@ pub fn subtract_constant(vec: &mut Vec<f64>, constant: f64) {
     for value in vec.iter_mut() {
         *value -= constant;
     }
+}
+
+#[allow(dead_code)]
+pub fn read_csv_to_two_vectors(path: &str) -> Result<(Vec<String>, Vec<f32>), Box<dyn Error>> {
+    let file = File::open(path)?;
+    let mut rdr = ReaderBuilder::new().from_reader(file);
+
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for result in rdr.records() {
+        let record = result?;
+        if record.len() >= 2 {
+            column1.push(record[0].to_string());
+            column2.push(record[1].parse::<f32>().unwrap_or(0.0));
+        }
+    }
+
+    Ok((column1, column2))
 }
