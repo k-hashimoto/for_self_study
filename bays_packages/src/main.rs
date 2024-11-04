5f9fd31e92e17bdb6e25bf584eab0e5c5561643f
k-hashimoto <kimiaki.hashi@gmail.com>
2024-11-04 11:24:39 +0900

    簡単な引数処理を追加

--- bays_packages/src/main.rs
+++ bays_packages/src/main.rs
@@ -1,18 +1,20 @@
-// mod modules;
-// mod calculation_plnans;
 mod modules;
 mod simple_mean_online;
-//use calculation_plnans::simple_mean_online;
 use std::env;
 
 // ---------------------------------------------------------------------
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // // 実行ファイル名を除いて、2つの引数が必要
-    // if args.len() < 2 {
-    //     eprintln!("Error: Missing arguments.\nUsage: {} <arg>", args[0]);
-    //     std::process::exit(1); // エラー終了コードを返す
-    // }
-    simple_mean_online::run();
+    let args: Vec<String> = env::args().collect();
+    // 実行ファイル名を除いて、2つの引数が必要
+    if args.len() < 2 {
+        eprintln!("Error: Missing arguments.\nUsage: {} <arg>", args[0]);
+        std::process::exit(1); // エラー終了コードを返す
+    }
+
+    if args[1] == "--simple_mean_online" { //cargo runを使う前提。cargo run -p bays_packages -- --simple_mean_online
+        simple_mean_online::run();
+    } else {
+        panic!("有効な引数ではありません!");
+    }
 }
 
