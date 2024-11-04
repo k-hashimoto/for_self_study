02ff87d307bf5562e91d720c1e8d2376acdb93f8
k-hashimoto <kimiaki.hashi@gmail.com>
2024-11-04 16:06:39 +0900

    安定した2

--- bays_packages/src/simple_mean_bulk.rs
+++ bays_packages/src/simple_mean_bulk.rs
@@ -2,9 +2,13 @@
 use crate::modules::utils::*;
 use crate::modules::mcmc_tools::*;
 use crate::modules::mcmc::*;
+// use crate::modules::mcmc_visualizer::*
+
+use plotters::prelude::*;
+use rand_distr::{Distribution, Normal, Uniform};
 
 // simple_mean_onlineのように逐次的にデータが入ってくるオンライン処理的な実装ではなく、stanで処理するときのようにバルクで処理する実装
-pub fn run() {
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     println!("[サンプル] ベイズ推定で顧客の平均購入金額を推定する(一括処理ver.)");
 
     // 目的：顧客の平均購入金額を推定する
@@ -17,6 +21,7 @@ pub fn run() {
                             35.0, 43.0, 53.0, 68.0, 48.0, 46.0, 44.0, 34.0, 48.0, 60.0
                         ];
 
+    let true_mean = mean_normal_dist(&observations);
     // 事前分布：平均1ドル、標準偏差100ドルの正規分布
     let mut prior_mu    = 50.0;
     let mut prior_sigma = 10.0;
@@ -29,26 +34,34 @@ pub fn run() {
     let thinning_interval = 10; // 薄化の間隔（例：10サンプルに1つを選択） 1000
     let burn_in = 50000; // バーンイン
     let proposal_scale = 5.0;// 提案分布のスケール
+
     // 初期値を異なる3つのチェーンで設定
-    let init_values = vec![30.0, 50.0, 70.0]; // 初期値の異なる設定
+    // 一様分布で初期値を生成
+    let mut rng = rand::thread_rng();
+    let uniform_dist = Uniform::new(true_mean - 10.0, true_mean + 10.0); // 初期値の範囲を20.0～80.0と設定
+    let init_values: Vec<f64> = (0..4).map(|_| uniform_dist.sample(&mut rng)).collect();
+    // let init_values = vec![30.0, 50.0, 70.0]; // 初期値の異なる設定
+
 
     println!("自己相関抑制のためのパラメータ");
     println!("  薄化インターバル : {}", thinning_interval);
     println!("  バーンイン : {}", burn_in);
 
     // 各初期値で独立したチェーンを実行
-    let mut all_samples = Vec::new();
 
+    let mut chains = Vec::new();
     // チェーンのループ
     for &init in init_values.iter() {
         // メトロポリス・ヘイスティングスの実行
         // MCMCの初期値を1つにすると、生成した数値の分散が早期にゼロになってしまう
         let samples = metropolis_hastings_bulk(iterations, burn_in, init, &observations, sigma, prior_mu, prior_sigma, proposal_scale);
-            // サンプルの薄化（間引き）
-        let thinned_samples = thin_samples(&samples, thinning_interval);
-        all_samples.extend(thinned_samples); // 結果を統合
+        // サンプルの薄化（間引き）
+        //let thinned_samples = thin_samples(&samples, thinning_interval);
+        chains.push(thin_samples(&samples, thinning_interval)); // 結果を統合
     }
 
+    // 平均や各種統計量の計算用
+    let all_samples: Vec<f64> = chains.iter().flat_map(|chain| chain.iter().cloned()).collect();
     // 事後分布の平均を計算
     posterior_mean = mean_normal_dist(&all_samples);
 
@@ -70,7 +83,43 @@ pub fn run() {
 
     println!("-------------------------------");
     println!("観測値(顧客の購入金額-ドル) : {:?}", observations);
-    println!("観測値の平均: {:.2}", mean_normal_dist(&observations));
+    println!("観測値の平均: {:.2}", true_mean);
     println!("ベイズ推定された事後分布の平均値: {:.2}", posterior_mean);
     println!("事後分布の平均値 - 単純平均: {:.2}", posterior_mean-mean_normal_dist(&observations));
+
+    // トレースプロットを生成してPNGに保存
+    let root = BitMapBackend::new("./plots/bays_packages/trace_plot.png", (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Trace Plot of MCMC Samples for 4 Chains(blue:1, red:2, green:3, purple:4)", ("sans-serif", 20))
+        .margin(20)
+        .x_label_area_size(30)
+        .y_label_area_size(40)
+        .build_cartesian_2d(0..((&iterations - &burn_in) / 10), 35.0..65.0)?;
+
+    chart.configure_mesh().draw()?;
+
+    // チェーンごとに異なる色でトレースプロットを描画
+    // let colors = [full_palette::BLUE, full_palette::RED, full_palette::GREEN, full_palette::PURPLE];
+    // チェーンごとの色を透明度付きで設定
+    let colors = [
+        RGBAColor(0, 0, 255, 0.2),    // 半透明の青
+        RGBAColor(255, 0, 0, 0.2),    // 半透明の赤
+        RGBAColor(0, 255, 0, 0.2),    // 半透明の緑
+        RGBAColor(128, 0, 128, 0.2),  // 半透明の紫
+    ];
+    for (i, chain) in chains.iter().enumerate() {
+        chart.draw_series(
+                LineSeries::new(
+                    chain.iter().enumerate().map(|(j, &value)| (j, value)),
+                    &colors[i],
+                )
+        )?;
+    }
+
+    // 保存処理の完了
+    root.present()?;
+    println!("Trace plot saved as 'plots/bays_packages/trace_plot.png'");
+    Ok(())
 }
